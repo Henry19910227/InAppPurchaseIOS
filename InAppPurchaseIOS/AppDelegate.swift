b966fe015c3c3a9c7325861fc41eb8ccaa51df89
Henry.Liao <Henry.Liao@skylinetw.com>
2021-07-27 17:35:19 +0800

    feat(appdelegate): 添加 Transaction Observer

--- InAppPurchaseIOS/AppDelegate.swift
+++ InAppPurchaseIOS/AppDelegate.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import StoreKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, SKPaymentTransactionObserver {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        SKPaymentQueue.default().add(self)
         return true
     }
 
@@ -29,6 +28,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        SKPaymentQueue.default().remove(self)
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
     }
 
 
